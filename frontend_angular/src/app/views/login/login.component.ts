8086ada435173a37b19eac41927b480b089f06da
josefernandogithub <josfrnando@gmail.com>
2023-03-14 19:43:54 -0300

    adicionando db.jsom

--- frontend_angular/src/app/views/login/login.component.ts
+++ frontend_angular/src/app/views/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
-import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-login',
--- frontend_angular/src/main.ts
+++ frontend_angular/src/main.ts
@@ -1,8 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as $ from 'jquery';
 import { AppModule } from './app/app.module';
 
 
+declare var $:any;
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
